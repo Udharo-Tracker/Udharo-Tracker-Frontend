e75d930cdfa8adc98bd0548c13e05538638af3c4
Manisha Magar <magarmanisha248@gmail.com>
2026-08-06 22:42:27 +0545

    chore: deleted files from the ts with .d.ts

--- src/types/credit-score.ts
+++
@@ -1,9 +0,0 @@
-export type CreditRiskLevel = "green" | "yellow" | "red";
-
-export interface CreditScore {
-  id: string;
-  customer: string;
-  calculated_at: string;
-  score: number;
-  risk_level: CreditRiskLevel;
-}
