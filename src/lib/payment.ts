2b350db6d8075395a0084f434df763fd676c2911
Manisha Magar <magarmanisha248@gmail.com>
2026-08-09 21:09:04 +0545

    chore: extract PAYMENT_MODE_LABELS into shared lib/payment

---
+++ src/lib/payment.ts
@@ -0,0 +1,7 @@
+export const PAYMENT_MODE_LABELS: Record<string, string> = {
+  cash: "Cash",
+  card: "Card",
+  fonepay: "Fonepay",
+  nepal_pay: "Nepal Pay",
+  bank_transfer: "Bank Transfer",
+};
